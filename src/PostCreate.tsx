7e1f58cf2547380f96a0bbb8b83e72e729e3e7cb
Rade58 <bajic.rade2@gmail.com>
2021-03-07 20:46:01 +0100

    👾

--- src/PostCreate.tsx
+++ src/PostCreate.tsx
@@ -1,11 +1,32 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useCallback } from "react";
+import axios from "axios";
 
 const PostCreate: FC = () => {
   const [title, setTitle] = useState<string>("");
 
+  const submitCallback = useCallback(async () => {
+    const createdPost = await axios.post(
+      "http://localhost:4000/posts",
+      {
+        title,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log({ createdPost });
+  }, [title]);
+
   return (
     <div>
-      <form>
+      <form
+        onSubmit={() => {
+          submitCallback();
+        }}
+      >
         <div className="form-group">
           <label htmlFor="title-input">Title</label>
           <input
@@ -18,7 +39,9 @@ const PostCreate: FC = () => {
             id="title-input"
             className="form-control"
           />
-          <button className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </div>
       </form>
     </div>
