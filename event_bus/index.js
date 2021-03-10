6fc46364591ddc65afdcbc4b79527f74202074e3
Rade58 <bajic.rade2@gmail.com>
2021-03-10 12:53:32 +0100

    👾

---
+++ event_bus/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const axios = require("axios");
+const { json, urlencoded } = require("body-parser");
+const cors = require("cors");
+const { Console } = require("node:console");
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.post("/events", async (req, res) => {
+  // TAKING EVENT
+  const event = req.body;
+
+  // SENDING NOTIFICATIONS TO ALL SERVICES
+
+  // TO POSTS SERVICE
+  try {
+    const response = await axios.post("http://localhost/4000/events", event);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // TO COMMENTS SERVICE
+  try {
+    const response = await axios.post("http://localhost/4001/events", event);
+  } catch (err) {
+    console.error(err);
+  }
+  // TO QUERY SERVICE
+  try {
+    const response = await axios.post("http://localhost/4002/events", event);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // JUST TO INDICATE THAT EVERYTHING IS WORKING AS EXPECTED
+  res.send({ status: "OK" });
+});
+
+const port = 4005;
+
+app.listen(port, () => {
+  console.log(`Event Bus on: http://localhost:${port}`);
+});
