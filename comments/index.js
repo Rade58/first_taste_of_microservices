6f8a06aa5d1e8a9aab2d8a4061132286faecdf4a
Rade58 <bajic.rade2@gmail.com>
2021-03-10 13:51:39 +0100

    👾

--- comments/index.js
+++ comments/index.js
@@ -3,21 +3,22 @@ const { json, urlencoded } = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 
+// TAKING axios
+const axios = require("axios");
+//
+
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-// ANOTHER IN MEMORY DATABASE
-//
 const commentsByPostId = {
   "post id": {
     id: "post id",
-    // ARRAY OF COMMENTS ASSOCIATED WIT A POST
+
     comments: [
       {
-        // comment id
         id: "",
         content: "",
       },
