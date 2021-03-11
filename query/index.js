0cef3340698340cb2da77722f3e3bf2f6332ca1c
Rade58 <bajic.rade2@gmail.com>
2021-03-11 11:09:51 +0100

    👾

--- query/index.js
+++ query/index.js
@@ -7,14 +7,28 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-// DEFINE GET (FOR ALL POSTS TOGETHER WITH COMMENTS)
+// FIRS LETS ADD DATA STRUCTURE FOR STORING STUFF
+// A FAKE DATABASE, BECAUSE I AM DOING THINGS IN MEMORY
+// FOR THIS PROJECT (I DON'T HAVE DATABASE)
+const posts = {
+  // THIS IS DICTIONARY
+  // AND ONE DOCUMENT WOULD LOOK LIKE THIS
+  /* "placeholder post Id": {
+    id: "same post id",
+    title: "posts title",
+    comments: [
+      // comments associated with one post
+      { id: "comment id", content: "stuff" },
+    ],
+  }, */
+};
+
 app.get("/posts", async (req, res) => {
-  // I'LL LEAVE THIS EMPTY FOR NOW
+  // -
 });
 
-// SO THIS IS GOING TO BE A HANDLER FOR ANTICAPATING
-// EVERY EVENT SENT BY EVENT BUS
-app.post("/posts", async (req, res) => {
+// LETS HANDLE EVENTS
+app.post("/events", async (req, res) => {
   // I'LL LEAVE THIS EMPTY FOR NOW
 });
 
