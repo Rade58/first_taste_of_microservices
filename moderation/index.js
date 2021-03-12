a8dccc2b0ce793c314ba65b778d1148f10269d78
Rade58 <bajic.rade2@gmail.com>
2021-03-12 14:52:41 +0100

    👾

--- moderation/index.js
+++ moderation/index.js
@@ -4,10 +4,32 @@ const { json } = require("body-parser");
 
 const app = express();
 
+// WE HAVE ANOTHER DATBASE
+// THIS WILL BE A DATABSE OF COMMNTS
+
+const comments = {
+  /* "comment id": {
+    id: "comment id",
+    content: "",
+    postId: ""
+  } */
+};
+
 app.use(json());
 
+// ANTICIPATING ECHO FROM EVENT BUS
 app.post("/events", async (req, res) => {
   const { type, payload } = req.body;
+
+  // TYPE WE ARE ANTICIPATING IS COMMENT CREATION
+  if (type === "CommentCreation") {
+    // IN HERE I WILL DO MODERATION
+    // AFTER WE ARE DONE WITH MODERATION
+    // WE SHOULD EMMIT "CommentModerated"
+    // WITH AXIOS
+    // AND "CommentModerated" WILL BE ANTICIPATED BY COMMENTS
+    // SERVICE
+  }
 });
 
 const port = 4003;
